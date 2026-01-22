d74a9e6c0837ca7275481a7731c0676abf2eb17f
wylcloud <82373611+wylcloud@users.noreply.github.com>
2026-01-22 16:48:24 +0800

    Update store-redirect3.js

--- store-redirect3.js
+++ store-redirect3.js
@@ -4,8 +4,18 @@ body = body.replace(
   /<\/body>/,
   `<script>
     document.addEventListener("DOMContentLoaded", function () {
-  let win = window.open("about:blank", "_blank");
-  win.location.href = "https://app.kaze.network/cart.php?a=checkout";
+  let btn = document.createElement("button");
+  btn.style.position = "fixed";
+  btn.style.opacity = 0;
+  btn.style.pointerEvents = "none";
+  document.body.appendChild(btn);
+
+  btn.addEventListener("click", function() {
+    window.open("https://app.kaze.network/cart.php?a=checkout", "_blank");
+  });
+
+  // 模拟用户点击
+  btn.click();
 });
 
   </script></body>`
